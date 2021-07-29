62601a0d47c25baeb6bebb33d6a395493223fb13
Jonathan <jonathanfernandezfm@gmail.com>
2021-07-29 22:15:44 +0200

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '73e2f6f4efcb7336262ac5219b911943'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ace83cea-c5ca-4b81-bcd0-11e94801449b'
+};
